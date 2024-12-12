972fef15df11025353c9b793b74390e41428c083
thanh2103 <thanh1thuy2003@gmail.com>
2024-12-12 09:01:23 +0700

    đã update3

--- src/src/Ss1_Java_Overview/Exercise8.java
+++ src/src/Ss1_Java_Overview/Exercise8.java
@@ -3,7 +3,7 @@ package Ss1_Java_Overview;
 public class Exercise8 {
     public static void main(String[] args) {
         int i=1;
-        System.out.println(i++);//1
-        System.out.println(++i);//3
+        System.out.println(i++);
+        System.out.println(++i);
     }
 }
