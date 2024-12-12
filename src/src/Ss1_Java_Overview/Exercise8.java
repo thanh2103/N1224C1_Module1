7b67eac9a50d835de3247acdbf1486269370ffce
thanh2103 <thanh1thuy2003@gmail.com>
2024-12-12 08:58:40 +0700

    đã update1

--- src/src/Ss1_Java_Overview/Exercise8.java
+++ src/src/Ss1_Java_Overview/Exercise8.java
@@ -1,4 +1,9 @@
 package Ss1_Java_Overview;
 
 public class Exercise8 {
+    public static void main(String[] args) {
+        int i=1;
+        System.out.println(i++);
+        System.out.println(++i);
+    }
 }
