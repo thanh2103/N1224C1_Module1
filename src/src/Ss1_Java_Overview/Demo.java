60c4dfe597fc2c0c7df8eeda466f07d86b99c540
thanh2103 <thanh1thuy2003@gmail.com>
2024-12-12 08:39:14 +0700

    đã update1

--- src/src/Ss1_Java_Overview/Demo.java
+++
@@ -1,8 +0,0 @@
-package Ss1_Java_Overview;
-
-public class Demo {
-    public static void main(String[] args) {
-        System.out.println("Hello World");
-        System.out.println("Hello World");
-    }
-}
