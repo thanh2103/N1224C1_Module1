0a2e0abc6738d34ad1f1cf09338efff214992ba3
thanh2103 <thanh1thuy2003@gmail.com>
2024-12-12 08:37:26 +0700

    đã update

--- src/src/Ss1_Java_Overview/Demo.java
+++ src/src/Ss1_Java_Overview/Demo.java
@@ -3,5 +3,6 @@ package Ss1_Java_Overview;
 public class Demo {
     public static void main(String[] args) {
         System.out.println("Hello World");
+        System.out.println("Hello World");
     }
 }
