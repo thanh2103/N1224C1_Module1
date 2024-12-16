d7f3d267d3bac6fdd968cf93dc19f120ff6515ef
thanh2103 <thanh1thuy2003@gmail.com>
2024-12-16 10:38:53 +0700

    đã update7

--- src/src/ss3_java_overview/Exercise11C2.java
+++ src/src/ss3_java_overview/Exercise11C2.java
@@ -2,25 +2,18 @@ package ss3_java_overview;
 
 import java.util.Scanner;
 
-public class Exercise11 {
+public class Exercise11C2 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Nhập n: ");
         int n = scanner.nextInt();
 
         double sum = 0.0;
-
-        // n=4
-        //i=1<4 | i=1=1 |
+        int fact = 1;
         for (int i = 1; i <= n; i++) {
-            int fact = 1;
-
-            for (int j = 1; j <= (2 * i - 1); j++) {
-                fact *= j;
-            }
-
             sum += 1.0 / fact;
+            fact *= (2 * i) * (2 * i + 1);
         }
-        System.out.println("Kết quả: " + sum);
+        System.out.println("Tổng s= "+sum);
     }
 }
